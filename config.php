f8be7b6e45001db6ec441c4c071ed45f5020686b
Mycke Ramos <mramosmanhaes@gmail.com>
2016-03-06 12:14:33 -0300

    Resets app config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-    define('BASE_URL', "http://" . $_SERVER['SERVER_NAME'] . '/abril-teste/');
+    define('BASE_URL', "http://" . $_SERVER['SERVER_NAME']);
 
     // Database Config
     define('DB_HOST', 'localhost');
-    define('DB_NAME', 'controle_estoque');
+    define('DB_NAME', 'dbname');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
